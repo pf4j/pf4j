f93ef728d640c0ed98a0ccceabd2d214f599a121
Decebal Suiu <decebal.suiu@gmail.com>
2013-05-17 01:20:44 +0300

    added run-demo.bat

---
+++ run-demo.bat
@@ -0,0 +1,26 @@
+REM
+REM This script creates and run the pf4j demo.
+REM
+
+REM create artifacts using maven
+call mvn clean package
+
+REM create demo-dist folder
+rmdir demo-dist /s /q
+mkdir demo-dist
+mkdir demo-dist\plugins
+
+REM copy artifacts to demo-dist folder
+xcopy demo\app\target\pf4j-demo-app-*.zip demo-dist /s /i
+xcopy demo\plugin1\target\pf4j-demo-plugin1-*.zip demo-dist\plugins /s
+xcopy demo\plugin2\target\pf4j-demo-plugin2-*.zip demo-dist\plugins /s
+
+cd demo-dist
+
+REM unzip app
+unzip pf4j-demo-app-*.zip
+rm pf4j-demo-app-*.zip
+
+REM run demo
+java -jar pf4j-demo-app-*-SNAPSHOT.jar
+cd ..
