a6b782623da7e65a3f8caf094d3e6b0b0ed34458
Alexander Horuzhiy <123456>
2016-01-18 16:31:54 +0100

    Remove Java 8 dependency

--- pf4j/src/main/java/ro/fortsoft/pf4j/ServiceProviderExtensionFinder.java
+++ pf4j/src/main/java/ro/fortsoft/pf4j/ServiceProviderExtensionFinder.java
@@ -28,7 +28,6 @@ import java.net.URL;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.stream.Stream;
 
 /**
  * The ServiceLoader base implementation for ExtensionFinder.
