c01f9726b1a48fa275bd76b46bb4367c3a98195f
Emmanuel Lecomte <emmanuel.lecomte@zenika.com>
2016-09-14 14:38:28 +0200

    Add inheritance support on Extension annot

--- pf4j/src/main/java/ro/fortsoft/pf4j/Extension.java
+++ pf4j/src/main/java/ro/fortsoft/pf4j/Extension.java
@@ -19,6 +19,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -27,6 +28,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RUNTIME)
 @Target(TYPE)
+@Inherited
 @Documented
 public @interface Extension {
 
