443191d0cc75079099278596c686c29d18cf9a01
Anindya Chatterjee <anidotnet@gmail.com>
2017-06-09 16:14:27 +0530

    Kotlin plugin example added and README updated for Kotlin. (#146)

---
+++ demo_gradle/plugins/plugin3/src/main/kotlin/ro/fortsoft/pf4j/demo/kotlin/KotlinPlugin.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ro.fortsoft.pf4j.demo.kotlin
+
+import org.apache.commons.lang3.StringUtils
+import ro.fortsoft.pf4j.Extension
+import ro.fortsoft.pf4j.Plugin
+import ro.fortsoft.pf4j.PluginWrapper
+import ro.fortsoft.pf4j.demo.api.Greeting
+
+/**
+ * A sample plugin written in Kotlin
+ *
+ * @author Anindya Chatterjee
+ */
+class KotlinPlugin(wrapper: PluginWrapper) : Plugin(wrapper) {
+
+    override fun start() {
+        println("KotlinPlugin.start()")
+        println(StringUtils.upperCase("KotlinPlugin"))
+    }
+
+    override fun stop() {
+        println("KotlinPlugin.stop()")
+    }
+}
+
+@Extension
+class KotlinGreeting : Greeting {
+    override fun getGreeting(): String {
+        return "KotlinGreetings"
+    }
+}
