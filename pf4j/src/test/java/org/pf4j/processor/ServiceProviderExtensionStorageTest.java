cde9506f97601b8569f0e78533e834aa9981dd2b
josiahhaswell <josiah.haswell@gmail.com>
2017-11-01 12:11:48 -0600

    Adding more tests like a good citizen

--- pf4j/src/test/java/org/pf4j/processor/ServiceProviderExtensionStorageTest.java
+++ pf4j/src/test/java/org/pf4j/processor/ServiceProviderExtensionStorageTest.java
@@ -2,13 +2,19 @@ package org.pf4j.processor;
 
 import org.junit.Test;
 
+import javax.annotation.processing.Filer;
+import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 import java.io.IOException;
 import java.io.StringReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
 
 /**
  * @author Josiah Haswell
@@ -23,7 +29,39 @@ public class ServiceProviderExtensionStorageTest {
 
         assertThat(entries.size(), is(1));
         assertThat(entries.contains("World"), is(true));
+    }
 
+
+    @Test
+    public void ensureReadingExtensionsProducesCorrectListOfExtensions() {
+        final StringReader file = new StringReader("#hello\n    World");
+        final ExtensionAnnotationProcessor processor = mock(ExtensionAnnotationProcessor.class);
+        final Map<String, Set<String>> extensions = new HashMap<>();
+        extensions.put("hello", Collections.singleton("world"));
+
+        given(processor.getExtensions()).willReturn(extensions);
+        ServiceProviderExtensionStorage extensionStorage = new ServiceProviderExtensionStorage(processor) {
+            @Override
+            protected Filer getFiler() {
+                try {
+                    Filer filer = mock(Filer.class);
+                    FileObject fileObject = mock(FileObject.class);
+                    given(fileObject.openReader(true)).willReturn(file);
+                    given(filer.getResource(
+                        any(StandardLocation.class),
+                        any(String.class),
+                        any(String.class)
+                    )).willReturn(fileObject);
+                    return filer;
+                } catch(IOException ex) {
+                    throw new IllegalStateException("Shouldn't have gotten here");
+                }
+            }
+        };
+
+        Map<String, Set<String>> read = extensionStorage.read();
+        assertThat(read.containsKey("hello"), is(true));
+        assertThat(read.get("hello"), is(Collections.singleton("World")));
     }
 
 }
