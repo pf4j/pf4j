b774c75e815ac063534f324dc2ff79d76f3ba6c0
Lee Painter <lee@javassh.com>
2020-02-19 15:56:47 +0000

    Further commit for pull request #365 Improve tests to check for dependency only resources, dependency and parent resources, and resources in all cloassloaders. This has been completed for both parent first and parent last classloader strategies. Fixed a few issues disovered during the tests to ensure duplicate resources are not returned.

--- pf4j/src/main/java/org/pf4j/PluginClassLoader.java
+++ pf4j/src/main/java/org/pf4j/PluginClassLoader.java
@@ -15,19 +15,19 @@
  */
 package org.pf4j;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * One instance of this class should be created by plugin manager for every available plug-in.
@@ -196,15 +196,24 @@ public class PluginClassLoader extends URLClassLoader {
 
             log.trace("Couldn't find resource '{}' in parent", name);
 
+            url = findResourceFromDependencies(name);
+           
+            if (url != null) {
+               log.trace("Found resource '{}' in dependencies", name);
+               return url;
+            }  
+            
             return findResource(name);
         }
     }
 
     @Override
     public Enumeration<URL> getResources(String name) throws IOException {
-        if (!parentFirst) {
-            List<URL> resources = new ArrayList<>();
+        
+    	List<URL> resources = new ArrayList<>();
 
+    	if (!parentFirst) {
+            
             resources.addAll(Collections.list(findResources(name)));
 
             resources.addAll(findResourcesFromDependencies(name));
@@ -213,10 +222,18 @@ public class PluginClassLoader extends URLClassLoader {
                 resources.addAll(Collections.list(getParent().getResources(name)));
             }
 
-            return Collections.enumeration(resources);
         } else {
-            return super.getResources(name);
+        	
+        	if (getParent() != null) {
+                resources.addAll(Collections.list(getParent().getResources(name)));
+            }
+        	
+        	resources.addAll(findResourcesFromDependencies(name));
+        	
+        	resources.addAll(Collections.list(super.findResources(name)));
         }
+    	
+    	return Collections.enumeration(resources);
     }
 
     private Class<?> loadClassFromDependencies(String className) {
@@ -244,14 +261,14 @@ public class PluginClassLoader extends URLClassLoader {
         log.trace("Search in dependencies for resource '{}'", name);
         List<PluginDependency> dependencies = pluginDescriptor.getDependencies();
         for (PluginDependency dependency : dependencies) {
-            ClassLoader classLoader = pluginManager.getPluginClassLoader(dependency.getPluginId());
+            PluginClassLoader classLoader = (PluginClassLoader) pluginManager.getPluginClassLoader(dependency.getPluginId());
 
             // If the dependency is marked as optional, its class loader might not be available.
             if (classLoader == null || dependency.isOptional()) {
                 continue;
             }
 
-            URL url = classLoader.getResource(name);
+            URL url = classLoader.findResource(name);
             if (Objects.nonNull(url)) {
             	return url;
             }
@@ -260,22 +277,20 @@ public class PluginClassLoader extends URLClassLoader {
         return null;
     }
     
-    private Collection<URL> findResourcesFromDependencies(String name) {
+    private Collection<URL> findResourcesFromDependencies(String name) throws IOException {
         log.trace("Search in dependencies for resources '{}'", name);
         List<URL> results = new ArrayList<>();
         List<PluginDependency> dependencies = pluginDescriptor.getDependencies();
         for (PluginDependency dependency : dependencies) {
-            ClassLoader classLoader = pluginManager.getPluginClassLoader(dependency.getPluginId());
+            PluginClassLoader classLoader = (PluginClassLoader) pluginManager.getPluginClassLoader(dependency.getPluginId());
 
             // If the dependency is marked as optional, its class loader might not be available.
             if (classLoader == null || dependency.isOptional()) {
                 continue;
             }
 
-            URL url = classLoader.getResource(name);
-            if (Objects.nonNull(url)) {
-            	results.add(url);
-            }
+            results.addAll(Collections.list(classLoader.findResources(name)));
+            
         }
 
         return results;
