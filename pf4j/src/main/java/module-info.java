fc0b55991b478dd8f36a51473df80cd38147b832
John Niang <johnniang@foxmail.com>
2024-01-30 00:18:33 +0800

    Upgrade Java SemVer to 0.10.2

--- pf4j/src/main/java/module-info.java
+++ pf4j/src/main/java/module-info.java
@@ -34,7 +34,7 @@ module org.pf4j {
     // The java-semver library currently does not provide a module.
     // Maybe we should send them a pull request, that at least they provide an
     // automatic module name in their MANIFEST file.
-    requires java.semver;
+    requires com.github.zafarkhaja.semver;
 
     // Maybe we should reconsider the package hierarchy, that only classes are
     // exported, which are required by 3rd party developers.
