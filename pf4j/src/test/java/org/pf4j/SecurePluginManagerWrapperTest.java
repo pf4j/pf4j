58f363823038319338a99f14d5b6b452bc444bea
Decebal Suiu <decebal.suiu@gmail.com>
2024-02-04 01:09:16 +0200

    Add missing license file header

--- pf4j/src/test/java/org/pf4j/SecurePluginManagerWrapperTest.java
+++ pf4j/src/test/java/org/pf4j/SecurePluginManagerWrapperTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.pf4j;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -269,4 +284,5 @@ public class SecurePluginManagerWrapperTest {
         assertEquals(null, wrappedPluginManager.whichPlugin(pluginManager.getExtensionClasses(OTHER_PLUGIN_ID).get(0)));
         assertEquals(THIS_PLUGIN_ID, wrappedPluginManager.whichPlugin(pluginManager.getExtensionClasses(THIS_PLUGIN_ID).get(0)).getPluginId());
     }
+
 }
