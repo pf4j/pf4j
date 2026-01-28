0bdef6caf08acbf272f49d7c3c1424d7c60f1bda
Decebal Suiu <decebal.suiu@gmail.com>
2026-01-28 14:32:35 +0200

    Restore module-info.java accidentally deleted in 20c2f80 (fixes #648)

---
+++ pf4j/src/main/java/module-info.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Module descriptor for PF4J.
+ *
+ * @author Decebal Suiu
+ * @author Andreas Rudolph
+ */
+module org.pf4j {
+    requires java.base;
+
+    // provides javax.annotation
+    requires java.compiler;
+
+    // provided by the ASM library, use "requires static" since it's optional
+    requires static org.objectweb.asm;
+
+    requires org.slf4j;
+
+    // The java-semver library currently does not provide a module.
+    // Maybe we should send them a pull request, that at least they provide an
+    // automatic module name in their MANIFEST file.
+    requires com.github.zafarkhaja.semver;
+
+    // Maybe we should reconsider the package hierarchy, that only classes are
+    // exported, which are required by 3rd party developers.
+    exports org.pf4j;
+    exports org.pf4j.processor;
+}
